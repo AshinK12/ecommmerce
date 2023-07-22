37914de18d52b835f65be19faebcdbcb921324a6
ashin <ashinyuvi12@gmail.com>
2023-07-22 23:19:55 +0530

    hell

---
+++ ecommerceapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from . import views
+
+app_name = 'ecommerceapp'
+
+urlpatterns = [
+    path('', views.allproductcat, name='allproductcat'),
+    path('<slug:c_slug>/', views.allproductcat, name='product_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+]
