4dd60b63d219827d7db5fef2a4f50303ff8eb814
ashin <ashinyuvi12@gmail.com>
2023-07-24 13:00:46 +0530

    h

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.allproductcat, name='allproductcat'),
     path('<slug:c_slug>/', views.allproductcat, name='product_by_category'),
     path('<slug:c_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+     path('', views.home_page, name='home'),
 ]
--- search_app/urls.py
+++ search_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'search_app'
 urlpatterns = [
     path('', views.SearchResult, name='SearchResult'),
-    path('', views.home_page, name='home'),
+
     # Closing parenthesis was missing here
 ]
